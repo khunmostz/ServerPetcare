48334bedb0dc6f26164a5c491998528ef581a6ad
khunmostz <khunmostz24@gmail.com>
2022-07-14 00:24:17 +0700

    process post api location (window)

--- routes/api_location.js
+++ routes/api_location.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const locationdb = require("../firebase");
 
-router.get("/location", async (req, res) => {
+router.get("/get/locationdb", async (req, res) => {
   res.json({
     location: "thailand",
+  });
+});
+
+router.post("/create/locationdb", async (req, res) => {
+  const { location } = req.body;
+  await locationsDb.add(location);
+  res.json({
+    message: "create location successful",
   });
 });
 
