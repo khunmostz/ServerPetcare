c126ca4be0e6455cb064bb127bbd8d67925afd0c
khunmostz24 <khunmostz24@gmail.com>
2022-07-31 00:29:16 +0700

    update route delete

--- routes/api_location.js
+++ routes/api_location.js
@@ -91,72 +91,17 @@ router.post(
   }
 );
 
-router.put(
-  "/put/location/:id",
-  upload.single("fileImage"),
-  async (req, res) => {
-    const { id } = req.params;
-    const {
-      locationName,
-      locationLat,
-      locationLong,
-      locationDesc,
-      locationImage,
-    } = req.body;
-    var imageUrl = "";
-    // cloudinary.uploader
-    //   .upload(
-    //     req.file.path,
-    //     {
-    //       user_filename: true,
-    //       unique_filename: false,
-    //       folder: "locations",
-    //     },
-    //     (err, image) => {
-    //       if (err) {
-    //         console.log(err);
-    //       }
-    //     }
-    //   )
-    //   .then(async (result) => {
-    //     imageUrl = result.url;
-    //     console.log(imageUrl);
-
-    if (imageUrl == null) {
-      const data = await admin
-        .app()
-        .firestore()
-        .collection("locations")
-        .doc(id)
-        .get();
-      const locationValues = [];
-      locationValues.push(data.data());
-      console.log(locationValues["locationImage"]);
-    }
-
-    await admin.app().firestore().collection("locations").doc(id).update({
-      locationName: locationName,
-      locationLat: locationLat,
-      locationLong: locationLong,
-      locationDesc: locationDesc,
-      locationImage: imageUrl,
-    });
-    res.json({
-      message: "update success",
-      image: "same image",
-      location: {
-        locationName,
-        locationLat,
-        locationLong,
-        locationDesc,
-        locationImage: imageUrl,
-      },
-    });
-  }
-);
-//   }
-// );
-
-router.delete("delete/location/:id", async (req, res) => {});
+router.delete("delete/location/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await admin
+    .app()
+    .firestore()
+    .collection("locations")
+    .doc(id)
+    .delete();
+  res.json({
+    message: `delete location id:${id} success`,
+  });
+});
 
 module.exports = router;
